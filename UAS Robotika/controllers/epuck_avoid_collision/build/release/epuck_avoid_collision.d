a430eb13cc6a2f1bc36e50b45ab3d81056a81ade
Wahyu Mubarak S <117147246+whyumubarak@users.noreply.github.com>
2023-01-19 20:11:21 +0700

    Add files via upload

---
+++ UAS Robotika/controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
@@ -0,0 +1,6 @@
+build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/robot.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/types.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/nodes.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/distance_sensor.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/motor.h
---
+++ UAS Robotika/controllers/epuck_go_forward/build/release/epuck_go_forward.d
@@ -0,0 +1,5 @@
+build/release/epuck_go_forward.o: epuck_go_forward.c \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/robot.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/types.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/nodes.h \
+ D:\Kuliah\Semester\ 7\Robotika\Webots/include/controller/c/webots/motor.h
